1a30f45dfd0a3c544b5144c4fa285bc16ce19d1f
Elf <cypelf@proton.me>
2020-01-06 21:30:17 +0100

    rustdoc permet de récupérer l'index de la doc de rust pour l'instant uniquement, il faudra trouver comment faire pour avoir les résultats de la recherche après exécution du JS

--- commands/rustdoc.js
+++ commands/rustdoc.js
@@ -2,26 +2,42 @@ module.exports = {
 	name: 'rustdoc',
     description: "Permet de récupérer les résultats d'une recherche dans la documentation de Rust.",
     guildOnly: false,
-    args: false,
+    args: true,
+    usage: "[mot à rechercher]",
     category: "others",
 
     async help(bot, msg, helpEmbed) {
 		helpEmbed
-			.setDescription("Cette commande sert à supprimer le nombre de messages voulus.")
-			.addField("Procédure", "Cette commande s'utilise comme ceci : `" + config.prefix + this.name + " " + this.usage + "`");
+			.setDescription("Cette commande permet de faire une recherche dans la documentation de Rust.")
+			.addField("Procédure", "Cette commande s'utilise comme ceci : `" + bot.config.prefix + this.name + " " + this.usage + "`");
 		msg.channel.send(helpEmbed);
 	},
 
-    async execute (bot, msg) {
+    async execute (bot, msg, args) {
+        const recherche = args[0];
+        let HTMLParser = require('node-html-parser');
         let https = require('https');
         let options = {
-            host: 'doc.rust-lang.org',
-            path: '/std/index.html'
+            host: "doc.rust-lang.org",
+            path: "/std/index.html?search=" + recherche
         };
 
-        let request = https.get(options, res => {
-            if (res.statusCode != 200) return msg.channel.send(`Une erreur est survenue : ${res.statusCode}. Réessayez plus tard.`);
-            else return console.log(`Yes ! Victoire ! Status : ${res.statusCode}`);
+        let request = https.get(options, async (res) => {
+            if (res.statusCode != 200) return await msg.channel.send(`Une erreur est survenue : ${res.statusCode}. Réessayez plus tard.`);
+            res.setEncoding("utf8");
+
+            let output = "";
+
+            res.on("data", (chunk) => {
+                output += chunk
+            });
+
+            res.on("end", () => {
+                let root = HTMLParser.parse(output);
+                console.log(root.querySelector("#search"));
+            });
+
+            request.end();
         });
     }
 };
