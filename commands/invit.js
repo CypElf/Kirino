96f5018268e65233d151168065578bb3ec7a23d7
Elf <cypelf@proton.me>
2020-04-10 12:16:12 +0200

    rustdoc 1.41.1 -> 1.42.0 and invit now needs only admin perm

--- commands/invit.js
+++ commands/invit.js
@@ -8,7 +8,7 @@ module.exports = {
 	async execute(bot, msg) {
 		const Discord = require('discord.js');
         const emb = new Discord.RichEmbed()
-			.addField("Inviter le bot **Kirino** sur un serveur", "Le lien permettant de m'inviter sur un serveur est disponible **[ici](https://discordapp.com/oauth2/authorize?client_id=493470054415859713&scope=bot&permissions=2146958847)**")
+			.addField("Inviter le bot **Kirino** sur un serveur", "Le lien permettant de m'inviter sur un serveur est disponible **[ici](https://discordapp.com/oauth2/authorize?client_id=493470054415859713&scope=bot&permissions=8)**")
 			.setColor('#DFC900')
 			.setFooter("Requête de " + msg.author.username, msg.author.avatarURL);
 		msg.channel.send(emb);
