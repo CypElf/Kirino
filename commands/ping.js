44712b2b3f0adc4f2228f9b39a882ec1362f55b3
Elf <cypelf@proton.me>
2020-04-12 20:01:03 +0200

    added emojis to ping as well

--- commands/ping.js
+++ commands/ping.js
@@ -7,6 +7,6 @@ module.exports = {
 
 	async execute (bot, msg) {
 		let start = Date.now();
-		await msg.channel.send("ping").then(async(m) => await m.edit(`pong (${Date.now() - start} ms)`));
+		await msg.channel.send("ping <:kirinowhat:698923096752783401>").then(async(m) => await m.edit(`pong <:kirinoglad:698923046819594351> (${Date.now() - start} ms)`));
 	}
 };
