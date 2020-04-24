5c15a3ad88b195da7c14ad3f220640cdbdf48dda
Elf <cypelf@proton.me>
2020-04-24 16:20:39 +0200

    fixed invit footer

--- commands/invit.js
+++ commands/invit.js
@@ -10,7 +10,7 @@ module.exports = {
         const emb = new Discord.MessageEmbed()
 			.addField(__("invit_bot") + " **" + bot.user.username + "** " + __("on_a_server"), __("the_link_to_invit_me_is_available") + " **" + __("here") + "(https://discordapp.com/oauth2/authorize?client_id=493470054415859713&scope=bot&permissions=8)**")
 			.setColor('#DFC900')
-			.setFooter(__("request_from") + msg.author.username, msg.author.avatarURL);
+			.setFooter(__("request_from") + msg.author.username, msg.author.avatarURL());
 		msg.channel.send(emb);
 	}
 };
