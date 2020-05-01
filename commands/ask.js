54822b7cba4382a33fcd92260fd77a2cbbb26e29
Elf <cypelf@proton.me>
2020-05-01 09:26:54 +0200

    aded whisper no to answers, fixed ask index

--- commands/ask.js
+++ commands/ask.js
@@ -7,9 +7,11 @@ module.exports = {
     usage: "usage_ask",
 
     async execute (bot, msg, args) {
-        const possibleAnswers = [__("Yes."), __("No."), __("maybe"), __("surely"), __("probably"), __("surely_not"), __("not_at_all"), __("i_do_not_think"), __("you_should_take_a_break"), __("do_not_want_to_answer"), __("who_are_you"), __("clearly_not"), __("they_whisper_yes"), __("idk")]
+        const possibleAnswers = [__("Yes."), __("No."), __("maybe"), __("surely"), __("probably"), __("surely_not"), __("not_at_all"), __("i_do_not_think"), __("you_should_take_a_break"), __("do_not_want_to_answer"), __("who_are_you"), __("clearly_not"), __("they_whisper_yes"), __("they_whisper_no"), __("idk")]
 
-        const choice = Math.floor(Math.random() * (possibleAnswers.length + 1))
+        const choice = Math.floor(Math.random() * (possibleAnswers.length));
+
+        console.log(choice)
 
         msg.channel.send(possibleAnswers[choice])
     }
