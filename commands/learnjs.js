e125cc071047e2437faeec548d2905af30c0634e
Elf <cypelf@proton.me>
2020-05-18 15:59:03 +0200

    fixed learnjs title

--- commands/learnjs.js
+++ commands/learnjs.js
@@ -9,7 +9,7 @@ module.exports = {
 	async execute (bot, msg) {
         const Discord = require("discord.js")
         let linksEmbed = new Discord.MessageEmbed()
-            .setTitle(__("learn_js"))
+            .setTitle(__("learn_javascript"))
             .addField(__("english"), `[JavaScript.info](https://javascript.info/)\n[${__("complete_book")}](https://books.goalkicker.com/JavaScriptBook/)\n[${__("nodejs_book")}](https://books.goalkicker.com/NodeJSBook/)`)
             .addField(__("french"), `[${__("mozilla_guide")}](https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide)`)
             .setThumbnail("https://cdn.pixabay.com/photo/2015/04/23/17/41/javascript-736400_960_720.png")
