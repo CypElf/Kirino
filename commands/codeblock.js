9449471640e74bb5b42a446a7d7a62ef4fb7bb1e
Elf <cypelf@proton.me>
2020-05-20 10:14:37 +0200

    codeblock command added

---
+++ commands/codeblock.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: "codeblock",
+    description: "description_codeblock",
+    guildOnly: false,
+	args: false,
+    category: "programming",
+    aliases: ["code"],
+
+	async execute (bot, msg) {
+        const Discord = require("discord.js")
+        let linksEmbed = new Discord.MessageEmbed()
+            .setTitle(__("send_code"))
+            .setDescription(__("explanation_code"))
+            .setThumbnail("https://cdn.discordapp.com/attachments/698105563485044748/712574232147656704/712320080884793537.png")
+            .setImage("https://cdn.discordapp.com/attachments/698105563485044748/712577943666294814/unknown.png")
+            .setColor("#000000")
+            .setFooter(__("request_from") + msg.author.username, msg.author.displayAvatarURL())
+		msg.channel.send(linksEmbed)
+	}
+}
