573d0815be0f83c774c76933d27d893ce93969f7
Elf <cypelf@proton.me>
2020-05-24 22:10:04 +0200

    invit now use the new discord.com domain name

--- commands/invit.js
+++ commands/invit.js
@@ -8,7 +8,7 @@ module.exports = {
 	async execute(bot, msg) {
 		const Discord = require('discord.js')
         const emb = new Discord.MessageEmbed()
-			.addField(__("invit_bot") + " **" + bot.user.username + "** " + __("on_a_server"), __("the_link_to_invit_me_is_available") + " **" + __("here") + "(https://discordapp.com/oauth2/authorize?client_id=493470054415859713&scope=bot&permissions=8)**")
+			.addField(__("invit_bot") + " **" + bot.user.username + "** " + __("on_a_server"), __("the_link_to_invit_me_is_available") + " **" + __("here") + "(https://discord.com/oauth2/authorize?client_id=493470054415859713&scope=bot&permissions=8)**")
 			.setColor('#DFC900')
 			.setFooter(__("request_from") + msg.author.username, msg.author.avatarURL())
 		msg.channel.send(emb)
