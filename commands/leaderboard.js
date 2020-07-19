98ea7d42b03fc241d5c5e92afba49ceb38748436
Elf <cypelf@proton.me>
2020-07-19 16:42:40 +0200

    leaderboard fix

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -8,9 +8,9 @@ module.exports = {
     aliases: ["lb"],
 
     async execute (bot, msg, args) {
-        const xpActivationRequest = bot.db.prepare("SELECT enabled FROM xp_activations WHERE guild_id = ?")
+        const xpActivationRequest = bot.db.prepare("SELECT is_enabled FROM xp_metadata WHERE guild_id = ?")
         let isEnabled = xpActivationRequest.get(msg.guild.id)
-        if (isEnabled) isEnabled = isEnabled.enabled
+        if (isEnabled) isEnabled = isEnabled.is_enabled
 
         if (isEnabled) msg.channel.send(`${msg.guild.name}'s leaderboard is available at https://www.avdray.com/leaderboards?gid=${msg.guild.id}`)
         else msg.channel.send(`The XP system is currently disabled. You must first activate it with the command \`${bot.prefix}xp enable\`.`)
