60e555e236b1f02732d56a01f3dca106c7c7ef9b
Elf <cypelf@proton.me>
2020-07-28 22:13:51 +0200

    added user search by nickname

--- res/get_user.js
+++ res/get_user.js
@@ -2,9 +2,15 @@ function getUser(msg, args) {
     member = msg.mentions.members.first()
     if (member === undefined) {
         let usernameOrID = args.join(" ")
-        member = msg.guild.members.cache.array().find(currentMember => currentMember.user.username.toLowerCase() === usernameOrID.toLowerCase())
+        member = msg.guild.members.cache.array().find(currentMember => {
+            if (currentMember.nickname) return currentMember.nickname.toLowerCase() === usernameOrID.toLowerCase()
+            else return false
+        })
         if (member === undefined) {
-            member = msg.guild.members.cache.array().find(currentMember => currentMember.id === usernameOrID)
+            member = msg.guild.members.cache.array().find(currentMember => currentMember.user.username.toLowerCase() === usernameOrID.toLowerCase())
+            if (member === undefined) {
+                member = msg.guild.members.cache.array().find(currentMember => currentMember.id === usernameOrID)
+            }
         }
     }
 
