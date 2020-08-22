a61c5e0e48aff66d72be566ba198fc886a7ce7fc
Elf <cypelf@proton.me>
2020-08-22 16:39:46 +0200

    removed cypelf's rust video formation from learnrust

--- commands/learnrust.js
+++ commands/learnrust.js
@@ -11,7 +11,7 @@ module.exports = {
         let linksEmbed = new Discord.MessageEmbed()
             .setTitle(__("learn_rust"))
             .addField(__("english"), `[${__("official_book")}](https://doc.rust-lang.org/book/)\n[${__("official_course_by_example")}](https://doc.rust-lang.org/stable/rust-by-example/)\n[Are we game yet?](http://arewegameyet.com/) (${__("game_development")})\n[${__("documentation")}](https://doc.rust-lang.org/std/)`)
-            .addField(__("french"), `[${__("guillaume_gomez_blog")}](https://blog.guillaume-gomez.fr/Rust/)\n[${__("video_formation")}](https://www.youtube.com/playlist?list=PLFxge2NokeDu8tP7uDfy8VUZOIS0tADNp)`)
+            .addField(__("french"), `[${__("guillaume_gomez_blog")}](https://blog.guillaume-gomez.fr/Rust/)`)
 			.setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Rust_programming_language_black_logo.svg/1024px-Rust_programming_language_black_logo.svg.png")
             .setColor("#555555")
             .setFooter(__("request_from") + msg.author.username, msg.author.displayAvatarURL())
