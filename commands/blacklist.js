ab610f487af781b2d24ceee2e67b9dcf161ace0b
Elf <cypelf@proton.me>
2020-08-23 14:27:42 +0200

    added blacklist i18n

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -24,22 +24,22 @@ module.exports = {
         removeDeletedChannels(bot.db, msg.guild)
 
         if (arg === "remove") {
-            if (!msg.member.hasPermission("ADMINISTRATOR")) return msg.channel.send("you're missing perm to remove remove channels")
+            if (!msg.member.hasPermission("ADMINISTRATOR")) return msg.channel.send(__("missing_permissions_to_remove_channel"))
             const channelArg = args[1]
-            if (!channelArg) return msg.channel.send("precise channel to remove")
+            if (!channelArg) return msg.channel.send(__("precise_channel_to_remove"))
             
             const channel = getChannel(msg, args.slice(1))
-            if (!channel) return msg.channel.send(`bad channel ${__("kirino_pout")}`)
+            if (!channel) return msg.channel.send(`${__("bad_channel")} ${__("kirino_pout")}`)
 
             const spChannelRequest = bot.db.prepare("SELECT * FROM xp_channels WHERE guild_id = ? AND channel_id = ?")
             const channelRow = spChannelRequest.get(msg.guild.id, channel.id)
 
-            if (!channelRow) return msg.channel.send("channel not in db")
+            if (!channelRow) return msg.channel.send(__("channel_not_in_db"))
 
             const deletionChannelRequest = bot.db.prepare("DELETE FROM xp_channels WHERE guild_id = ? AND channel_id = ?")
             deletionChannelRequest.run(msg.guild.id, channel.id)
 
-            msg.channel.send("channel removed from db")
+            msg.channel.send(`${__("the_channel")} ${channel.name} ${__("has_been_removed_to_list")}`)
         }
         else if (arg === "list") {
             const channelsRows = channelRequest.all(msg.guild.id).map(row => row.channel_id)
@@ -47,31 +47,31 @@ module.exports = {
 
             const Discord = require("discord.js")
             const blacklistEmbed = new Discord.MessageEmbed()
-                .setTitle("Channels blacklisted")
+                .setTitle(__("channels_blacklisted"))
                 .setColor("#000000")
 
-            if (blacklistedChannels.length === 0) blacklistEmbed.setDescription("No channel blacklisted for the moment.")
+            if (blacklistedChannels.length === 0) blacklistEmbed.setDescription(__("no_blacklisted_channels"))
             else blacklistEmbed.setDescription(`\`${blacklistedChannels.join("`, `")}\``)
 
             msg.channel.send(blacklistEmbed)
         }
         else {
-            if (!msg.member.hasPermission("ADMINISTRATOR")) return msg.channel.send("missing perm to add channel")
+            if (!msg.member.hasPermission("ADMINISTRATOR")) return msg.channel.send(__("missing_perm_to_add_channel"))
 
             let channel = getChannel(msg, args)
 
-            if (!channel) return msg.channel.send(`bad channel ${__("kirino_pout")}`)
+            if (!channel) return msg.channel.send(`${__("bad_channel")} ${__("kirino_pout")}`)
 
             const channelsRows = channelRequest.all(msg.guild.id)
 
-            if (channelsRows.map(row => row.channel_id).filter(channel_id => channel_id === channel.id).length > 0) return msg.channel.send("channel already present")
+            if (channelsRows.map(row => row.channel_id).filter(channel_id => channel_id === channel.id).length > 0) return msg.channel.send(__("channel_already_present"))
 
-            if (channelsRows.length === 10) return msg.channel.send("max channels count reached")
+            if (channelsRows.length === 10) return msg.channel.send(__("max_channels_count_reached"))
 
             const addChannelRequest = bot.db.prepare("INSERT INTO xp_channels VALUES(?,?)")
             addChannelRequest.run(msg.guild.id, channel.id)
 
-            msg.channel.send(`the channel ${channel.name} has been added to list`)
+            msg.channel.send(`${__("the_channel")} ${channel.name} ${__("has_been_added_to_list")}`)
         }
     }
 }
