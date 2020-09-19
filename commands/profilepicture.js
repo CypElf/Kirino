3d2f523efdefc3ae3adfa87b5fd5bd484d0a3802
Elf <cypelf@proton.me>
2020-09-19 16:50:02 +0200

    profilepicture now also uses the get_user lib

--- commands/profilepicture.js
+++ commands/profilepicture.js
@@ -13,17 +13,11 @@ module.exports = {
         if (!args.length) member = msg.member
 
         else {
-            member = msg.mentions.members.first()
-            if (member === undefined) {
-                let usernameOrID = args.join(" ")
-                member = msg.guild.members.cache.array().find(currentMember => currentMember.user.username.toLowerCase() === usernameOrID.toLowerCase())
-                if (member === undefined) {
-                    member = msg.guild.members.cache.array().find(currentMember => currentMember.id === usernameOrID)
-                    if (member === undefined) {
-                        return msg.channel.send(`${__("please_correctly_write_or_mention_a_member")} ${__("kirino_pout")}`)
-                    }
-                }
-            }
+            const getUser = require("../lib/get_user")
+
+            member = await getUser(msg, args)
+            
+            if (member === undefined) return msg.channel.send(`${__("please_correctly_write_or_mention_a_member")} ${__("kirino_pout")}`)
         }
 
         msg.channel.send(member.user.displayAvatarURL({
