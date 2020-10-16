6c4ba2fc23fc0fb80de7619f67b5ff0d01a76b0c
Elf <cypelf@proton.me>
2020-10-16 20:58:33 +0200

    leaderboard link updated to kirino

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -16,6 +16,6 @@ module.exports = {
         let params = ""
             if (args[0]) params = `&limit=${args[0]}`
             if (args[1]) params += `&page=${args[1]}`
-            msg.channel.send(`${__("leaderboard_of")}${msg.guild.name}${__("is_available_at")} https://www.avdray.com/leaderboards?gid=${msg.guild.id}${params} ${__("kirino_glad")}`)
+            msg.channel.send(`${__("leaderboard_of")}${msg.guild.name}${__("is_available_at")} https://www.kirino.xyz/leaderboards?gid=${msg.guild.id}${params} ${__("kirino_glad")}`)
     }
 }
