1668ff04e207b3fcb3e440bb632ff72fa266e5c6
Elf <cypelf@proton.me>
2020-11-30 13:12:28 +0100

    fixed guild delete database clean

--- index.js
+++ index.js
@@ -143,9 +143,8 @@ bot.on("guildCreate", guild  => {
 bot.on("guildDelete", guild => {
     console.log(`Server left: ${guild.name}`)
 
-    for (const table of ["banwords", "languages", "prefixs", "rules", "presences", "joins_leaves", "xp_blacklisted_channels", "xp_blacklisted_roles", "xp_guilds", "xp_roles"]) {
-        bot.db.prepare(`DELETE FROM ${table} WHERE guild_id = ?`).run(guild.id)
-    }
+    for (const table of ["banwords", "rules", "presences", "joins_leaves", "xp_blacklisted_channels", "xp_blacklisted_roles", "xp_guilds", "xp_roles"]) bot.db.prepare(`DELETE FROM ${table} WHERE guild_id = ?`).run(guild.id)
+    for (const table of ["languages", "prefixs"]) bot.db.prepare(`DELETE FROM ${table} WHERE id = ?`).run(guild.id)
 
     updateActivity()
 })
