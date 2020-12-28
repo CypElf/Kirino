9b34efc42efe597bcfb144e936204f5de7d9a975
Elf <cypelf@proton.me>
2020-12-28 15:25:20 +0100

    rustdoc updated to rust 1.48.0

--- commands/rustdoc.js
+++ commands/rustdoc.js
@@ -13,55 +13,41 @@ module.exports = {
         const rustDocResearcher = require("../lib/rustdoc/rustdoc_researcher")
 
         const keywords = args.join(" ")
-        const results = rustDocResearcher(keywords)
-        
-        let counter = 0
+        let results = rustDocResearcher(keywords)
+        // need to apply a fix to the researcher : after first command usage, some entries are not returned by the researcher and some undefined are put in URLs
+
         let contentNames = ""
         let contentIsArgs = ""
         let contentReturned = ""
 
-        for (let result of results.others) {
-            if (counter >= 5) break
+        for (let i = 0 ; i < 5 ; i++) {
+            const result = results.returned[i]
+            if (result === undefined) break
             contentNames += "- ["
-            if (result.path !== "") {
-                contentNames += result.path + "::"
-            }
-            contentNames += "**" + result.name + "**](" + result.href + ")"
-            if (result.desc !== "") {
-                contentNames += " : " + result.desc
-            }
+            if (result.path !== "") contentNames += result.path + "::"
+            contentNames += `**${result.name}**](${result.href})`
+            if (result.desc !== "") contentNames += " : " + result.desc
             contentNames += "\n"
-            counter++
         }
 
-        counter = 0
-        for (let result of results.in_args) {
-            if (counter >= 5) break
+        for (let i = 0 ; i < 5 ; i++) {
+            const result = results.returned[i]
+            if (result === undefined) break
             contentIsArgs += "- ["
-            if (result.path !== "") {
-                contentIsArgs += result.path + "::"
-            }
-            contentIsArgs += "**" + result.name + "**](" + result.href + ")"
-            if (result.desc !== "") {
-                contentIsArgs += " : " + result.desc
-            }
+            if (result.path !== "") contentIsArgs += result.path + "::"
+            contentIsArgs += `**${result.name}**](${result.href})`
+            if (result.desc !== "") contentIsArgs += " : " + result.desc
             contentIsArgs += "\n"
-            counter++
         }
 
-        counter = 0
-        for (let result of results.returned) {
-            if (counter >= 5) break
+        for (let i = 0 ; i < 5 ; i++) {
+            const result = results.returned[i]
+            if (result === undefined) break
             contentReturned += "- ["
-            if (result.path !== "") {
-                contentReturned += result.path + "::"
-            }
-            contentReturned += "**" + result.name + "**](" + result.href + ")"
-            if (result.desc !== "") {
-                contentReturned += " : " + result.desc
-            }
+            if (result.path !== "") contentReturned += result.path + "::"
+            contentReturned += `**${result.name}**](${result.href})`
+            if (result.desc !== "") contentReturned += " : " + result.desc
             contentReturned += "\n"
-            counter++
         }
 
         let embed = new Discord.MessageEmbed()
@@ -70,21 +56,10 @@ module.exports = {
 			.setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Rust_programming_language_black_logo.svg/1024px-Rust_programming_language_black_logo.svg.png")
             .setFooter(__("request_from") + msg.author.username, msg.author.displayAvatarURL())
 
-        if (contentNames !== "") {
-            embed.addField(__("in_name"), contentNames)
-        }
-
-        if (contentIsArgs !== "") {
-            embed.addField(__("in_settings"), contentIsArgs)
-        }
-
-        if (contentReturned !== "") {
-            embed.addField(__("in_return_types"), contentReturned)
-        }
-
-        if (contentNames === "" && contentIsArgs === "" && contentReturned === "") {
-            embed.addField(__("no_result_title"), __("no_result"))
-        }
+        if (contentNames !== "") embed.addField(__("in_name"), contentNames)
+        if (contentIsArgs !== "") embed.addField(__("in_settings"), contentIsArgs)
+        if (contentReturned !== "") embed.addField(__("in_return_types"), contentReturned)
+        if (contentNames === "" && contentIsArgs === "" && contentReturned === "") embed.addField(__("no_result_title"), __("no_result"))
         
         msg.channel.send(embed)
     }
