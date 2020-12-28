07b09169e55b0227cea745c403a0e01cf5358958
Elf <cypelf@proton.me>
2020-12-28 16:22:55 +0100

    updated some ask details

--- commands/ask.js
+++ commands/ask.js
@@ -8,7 +8,9 @@ module.exports = {
     usage: "usage_ask",
 
     async execute (bot, msg, args) {
-        const possibleAnswers = [__("Yes."), __("No."), __("maybe"), __("surely"), __("probably"), __("surely_not"), __("not_at_all"), __("i_do_not_think"), __("you_should_take_a_break"), __("do_not_want_to_answer"), __("who_are_you"), __("clearly_not"), __("they_whisper_yes"), __("they_whisper_no"), __("idk")]
+        const possibleAnswers = [__("Yes."), __("No."), __("maybe"), __("surely"), __("probably"), __("surely_not"), __("not_at_all"), __("i_do_not_think"), __("you_should_take_a_break"), __("do_not_want_to_answer"), __("clearly_not"), __("they_whisper_yes"), __("they_whisper_no"), __("idk")]
+
+        if (getLocale() === "fr") possibleAnswers.push(__("ptdr_t_ki"))
 
         const choice = Math.floor(Math.random() * (possibleAnswers.length));
 
