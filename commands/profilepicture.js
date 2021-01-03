846e50fa73050c8ce9f685cdafa41e7f31612659
Elf <cypelf@proton.me>
2021-01-03 20:59:24 +0100

    fixed pp error when cancelling undefined return case of getMember was forgotten

--- commands/profilepicture.js
+++ commands/profilepicture.js
@@ -12,9 +12,9 @@ module.exports = {
         else {
             const getMember = require("../lib/getters/get_member")
             user = await getMember(msg, args)
-            user = user.user
             
             if (user === undefined) return msg.channel.send(`${__("please_correctly_write_or_mention_a_member")} ${__("kirino_pout")}`)
+            user = user.user
         }
 
         msg.channel.send(user.displayAvatarURL({
