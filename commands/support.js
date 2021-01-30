2ff324ee2409052281f3d21e45561be2f9ee3319
Elf <cypelf@proton.me>
2021-01-30 18:34:55 +0100

    support command

---
+++ commands/support.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "support",
+    guildOnly: false,
+    category: "others",
+    cooldown: 1,
+
+    async execute (bot, msg) {
+        msg.channel.send("https://discord.gg/NNAGZCz")
+    }
+}
