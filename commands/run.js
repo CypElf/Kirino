5c8fe912da6b36232d788e5a65a59176ca17e19a
Elf <cypelf@proton.me>
2021-02-02 17:18:49 +0100

    run code input fixs

--- commands/run.js
+++ commands/run.js
@@ -1,3 +1,5 @@
+const { __ } = require("i18n")
+
 module.exports = {
 	name: "run",
     guildOnly: false,
@@ -5,6 +7,8 @@ module.exports = {
     category: "programming",
 
     async execute (bot, msg, args) {
+        if (args.length < 2 && args.join(" ").split("\n").length < 2) return msg.channel.send(`${__("run_need_two_args")} ${__("kirino_pout")}`)
+
         const { deflateSync } = require("zlib")
 
         const to_bytes = (str) => Buffer.from(str, "utf8")
@@ -65,16 +69,19 @@ module.exports = {
         }
 
         const language = args[0].split("\n")[0]
-        let code = args.join(" ").split("\n").slice(1).join("\n")
+
+        args = args.join(" ")
+        let code = args.split("\n").length > 1 ? args.split("\n").slice(1).join("\n") : args.split(" ").slice(1).join(" ")
 
         if (code.split("\n")[0].split(" ").length === 1 && code.startsWith("```")) code = code.split("\n").slice(1).join("\n")
+        else if (code.startsWith("```")) code = code.slice(3)
         if (code.endsWith("```")) code = code.slice(0, code.length - 3)
        
         const tio = new Tio(language, code)
 
         let data = await tio.send()
 
-        data = data.split("\n").slice(0, data.split("\n").length - 5).concat(data.split("\n")[data.split("\n").length - 1]).join("\n")
+        data = data.split("\n").filter(line => !line.startsWith("Real time") && !line.startsWith("User time") && !line.startsWith("Sys. time") && !line.startsWith("CPU share")).join("\n")
 
         msg.channel.send(data)
 	}
