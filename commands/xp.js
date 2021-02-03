72fdad71ff410a887579d8a92f7c30f5a38708b3
Elf <cypelf@proton.me>
2021-02-03 16:29:57 +0100

    fixed update background on 0 xp

--- commands/xp.js
+++ commands/xp.js
@@ -492,5 +492,8 @@ Canvas.CanvasRenderingContext2D.prototype.roundedRectangle = function(x, y, widt
 
 function updateBackground(db, msg, background) {
     const userRow = db.prepare("SELECT xp, total_xp, level FROM xp_profiles WHERE guild_id = ? AND user_id = ?").get(msg.guild.id, msg.author.id)
+    if (userRow.xp === undefined) userRow.xp = 0
+    if (userRow.total_xp === undefined) userRow.total_xp = 0
+    if (userRow.level === undefined) userRow.level = 0
     db.prepare("INSERT INTO xp_profiles(guild_id, user_id, xp, total_xp, level, background) VALUES(?,?,?,?,?,?) ON CONFLICT(guild_id, user_id) DO UPDATE SET background=excluded.background").run(msg.guild.id, msg.author.id, userRow.xp, userRow.total_xp, userRow.level, background)
 }
