27b5575fe69a5b85654bf75f41e89382b02c39a5
Elf <cypelf@proton.me>
2021-04-04 11:20:12 +0200

    added youtube search by title for play (need optimizations)

--- commands/play.js
+++ commands/play.js
@@ -9,33 +9,58 @@ module.exports = {
     async execute (bot, msg, args) {
         const url = args[0]
 
-        if (bot.voicesQueues.has(msg.guild.id)) {
+        if (bot.voicesQueues.has(msg.guild.id)) {            
+            let readable
+            let song
+            
             try {
-                const readable = await ytdl(url)
-                const videoInfo = await ytdl.getInfo(url)
-                const { author: { author_name, channel_url }, title, description, video_url, thumbnails } = videoInfo.videoDetails
+                readable = await ytdl(url)
 
-                const song = {
+                const videoInfo = await ytdl.getInfo(url)
+                const { author, title, description, video_url, thumbnails } = videoInfo.videoDetails
+
+                song = {
                     stream: readable,
                     url : video_url,
                     title: title,
                     description: description,
                     thumbnail: thumbnails[thumbnails.length - 1],
-                    author_name: author_name,
-                    channel_url: channel_url
+                    author_name: author.author_name,
+                    channel_url: author.channel_url
                 }
-
-                const serverQueue = bot.voicesQueues.get(msg.guild.id)
-                
-                serverQueue.songs.push(song)
-                
-                if (serverQueue.songs.length === 1) play(msg.channel, serverQueue)
-
-                else msg.channel.send("Added to the queue.")
             }
             catch {
-                msg.channel.send("Video not found.")
+                try {
+                    const yts = require("yt-search")
+
+                    const search = args.join(" ")
+                    const result = await yts(search)
+
+                    const { author, title, description, url, thumbnail } = result.videos[0]
+
+                    readable = await ytdl(url)
+
+                    song = {
+                        stream: readable,
+                        url : url,
+                        title: title,
+                        description: description,
+                        thumbnail: thumbnail,
+                        author_name: author.name,
+                        channel_url: author.url
+                    }
+                }
+                catch {
+                    return msg.channel.send("Video not found.")
+                }
             }
+
+            const serverQueue = bot.voicesQueues.get(msg.guild.id)
+            serverQueue.songs.push(song)
+
+            if (serverQueue.songs.length === 1) play(msg.channel, serverQueue)
+
+            else msg.channel.send("Added to the queue.")
         }
         else {
             msg.channel.send("I'm not in a voice channel.")
