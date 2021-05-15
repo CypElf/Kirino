bf8916e39c3f06e228843bc6c1d637d297d9ae28
Elf <cypelf@proton.me>
2021-05-15 15:58:29 +0200

    kirino auto disconnection fixed when someone go back to the vocal before the timeout is reached

--- events/voice_state_update.js
+++ events/voice_state_update.js
@@ -1,11 +1,14 @@
 module.exports = bot => {
-    const timeOut = 5 // in minutes
+    const timeOut = 0.2 // in minutes
 
     bot.on("voiceStateUpdate", async (oldState, newState) => {
+        console.log(oldState)
+        console.log(newState)
+        console.log(oldState.channelID === oldState.guild.me.voice.channelID && !newState.channel)
         if (oldState.channelID === oldState.guild.me.voice.channelID && !newState.channel) {
-            if (oldState.channel != null && !oldState.channel.members.size - 1) {
+            if (oldState.channel != null && !(oldState.channel.members.size - 1)) {
                 setTimeout(() => {
-                    if (!oldState.channel.members.size - 1) {
+                    if (!(oldState.channel.members.size - 1)) {
                         oldState.channel.leave()
                     }
                 }, timeOut * 60 * 1000)
