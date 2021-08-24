81a715fa8f7b8f57e3054d03bb0bc9061210769b
Elf <cypelf@proton.me>
2021-08-24 14:21:18 +0200

    fixs

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,19 +1,20 @@
 const resetJoin = require("../lib/joins_leaves/reset_leave")
+const formatJoinLeaveMessage = require("../lib/joins_leaves/format_join_leave_message")
 
 module.exports = bot => {
     bot.on("guildMemberAdd", async member => {
         const row = bot.db.prepare("SELECT joins_channel_id, join_message FROM joins_leaves WHERE guild_id = ?").get(member.guild.id)
 
         if (row) {
-            try {
+            // try {
                 const channel = await member.guild.channels.fetch(row.joins_channel_id)
                 const formatted = formatJoinLeaveMessage(row.join_message, member)
 
                 channel.send(formatted)
-            }
-            catch {
-                resetJoin(db, member.guild.id)
-            }
+            // }
+            // catch {
+            //     resetJoin(bot.db, member.guild.id)
+            // }
         }
     })
 }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,19 +1,20 @@
 const resetLeave = require("../lib/joins_leaves/reset_leave")
+const formatJoinLeaveMessage = require("../lib/joins_leaves/format_join_leave_message")
 
 module.exports = bot => {
     bot.on("guildMemberRemove", async member => {
         const row = bot.db.prepare("SELECT leaves_channel_id, leave_message FROM joins_leaves WHERE guild_id = ?").get(member.guild.id)
 
         if (row.leaves_channel_id) {
-            try {
+            // try {
                 const channel = await member.guild.channels.fetch(row.leaves_channel_id)
                 const formatted = formatJoinLeaveMessage(row.leave_message, member)
 
                 channel.send(formatted)
-            }
-            catch {
-                resetLeave(db, member.guild.id)
-            }
+            // }
+            // catch {
+            //     resetLeave(bot.db, member.guild.id)
+            // }
         }
     })
 }
