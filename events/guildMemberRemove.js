10df513bb38a036c357a51b2fc61655847ea5deb
Elf <cypelf@proton.me>
2021-08-24 15:10:56 +0200

    last fix I promise

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -5,7 +5,7 @@ module.exports = bot => {
     bot.on("guildMemberRemove", async member => {
         const row = bot.db.prepare("SELECT leaves_channel_id, leave_message FROM joins_leaves WHERE guild_id = ?").get(member.guild.id)
 
-        if (row.leaves_channel_id) {
+        if (row) {
             try {
                 const channel = await member.guild.channels.fetch(row.leaves_channel_id)
                 const formatted = formatJoinLeaveMessage(row.leave_message, member)
