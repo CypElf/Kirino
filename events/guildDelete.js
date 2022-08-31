59bd80dd796aee5a30a44e3664dd054953020064
Elf <cypelf@proton.me>
2022-08-31 11:28:36 +0200

    addded check for guild name before logging guildDelete events

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,8 +1,8 @@
 const updateActivity = require("../lib/misc/update_activity")
 
 module.exports = bot => {
-    bot.on("guildDelete", guild => {
-        console.log(`Server left: ${guild.name}`)
+    bot.on("guildDelete", async guild => {
+        if (guild.name) console.log(`Server left: ${guild.name}`)
 
         for (const table of ["banwords", "rules", "calls", "joins_leaves", "xp_blacklisted_channels", "xp_blacklisted_roles", "xp_guilds", "xp_roles"]) bot.db.prepare(`DELETE FROM ${table} WHERE guild_id = ?`).run(guild.id)
         for (const table of ["languages", "prefixs"]) bot.db.prepare(`DELETE FROM ${table} WHERE id = ?`).run(guild.id)
