23864906c6d942dcfc5e061cd6188857c5ff42f3
Elf <cypelf@proton.me>
2023-09-23 22:41:29 +0200

    removed guildDelete event prefixs table entry removal

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -6,7 +6,7 @@ export function eventHandler(bot: Kirino) {
         if (guild.name) console.log(`Server left: ${guild.name}`)
 
         for (const table of ["banwords", "rules", "calls", "joins_leaves", "xp_blacklisted_channels", "xp_blacklisted_roles", "xp_guilds", "xp_roles"]) bot.db.prepare(`DELETE FROM ${table} WHERE guild_id = ?`).run(guild.id)
-        for (const table of ["languages", "prefixs"]) bot.db.prepare(`DELETE FROM ${table} WHERE id = ?`).run(guild.id)
+        bot.db.prepare(`DELETE FROM languages WHERE id = ?`).run(guild.id)
 
         updateActivity(bot)
     })
