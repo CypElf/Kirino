45298dfc2d925985f75be262fe3a9b596a3013bb
Elf <cypelf@proton.me>
2023-12-05 16:39:13 +0100

    added http status codes to the API

--- src/index.ts
+++ src/index.ts
@@ -2,39 +2,12 @@ import { GatewayIntentBits } from "discord.js"
 import dotenv from "dotenv"
 import path from "path"
 import fs from "fs"
-import http from "http"
-import url from "url"
 import i18next from "i18next"
 import Backend from "i18next-fs-backend"
 import { Kirino, CommandFileObject } from "./lib/misc/types"
-import { XpProfile } from "./lib/misc/database"
+import { startXpApi } from "./lib/api/api"
 
 dotenv.config()
-
-type ApiQuery = {
-    id?: string,
-    limit?: number,
-    page?: number
-}
-
-type ApiData = {
-    guild_metadata: {
-        id: string,
-        name: string,
-        icon: string | null,
-        players: number
-    },
-    players: {
-        id: string,
-        tag: string,
-        avatar: string,
-        xp: number,
-        total_xp: number,
-        level: number,
-        color: string | undefined,
-        rank: number
-    }[]
-}
 
 const bot = new Kirino({ intents: [
     GatewayIntentBits.Guilds,
@@ -59,8 +32,6 @@ async function main() {
         }
     })
 
-    startXpApi(bot.apiCooldowns)
-
     const eventsFiles = fs.readdirSync(path.join(__dirname, "events")).filter(file => file.endsWith(".js"))
     for (const file of eventsFiles) {
         const { eventHandler } = await import(path.join(__dirname, "events", file))
@@ -84,138 +55,9 @@ async function main() {
     })
 
     if (!process.env.KIRINO_TOKEN) throw new Error("No token provided, please check your env.")
-    bot.login(process.env.KIRINO_TOKEN)
+    await bot.login(process.env.KIRINO_TOKEN)
+
+    startXpApi(bot)
 }
 
 main()
-
-// ------------------------------------------------------------- utility functions
-
-function controlRequest(req: http.IncomingMessage, res: http.ServerResponse, cooldowns: Map<string, number>, cooldown: number) {
-    res.setHeader("Content-Type", "application/json")
-    if (req.headers.authorization !== process.env.API_TOKEN) {
-        res.writeHead(403) // HTTP status code 403 = Forbidden
-        res.write(JSON.stringify({ "errors": ["Invalid authentification token."] }))
-        res.end()
-        return false
-    }
-
-    const now = Date.now()
-    const ip = req.socket.remoteAddress
-
-    if (!ip) return false
-
-    const timestamp = cooldowns.get(ip)
-    if (timestamp !== undefined) {
-        const expiration = timestamp + cooldown
-
-        if (now < expiration) {
-            res.writeHead(429) // HTTP status code 429 = Too Many Requests
-            res.write(JSON.stringify({ "errors": ["Too many requests. Please stop sending requests that fast."] }))
-            res.end()
-            return false
-        }
-    }
-
-    cooldowns.set(ip, now)
-    setTimeout(() => cooldowns.delete(ip), cooldown)
-    return true
-}
-
-function startXpApi(cooldowns: Map<string, number>) {
-    http.createServer(async (req, res) => {
-        if (!req.url) return res.end()
-
-        console.log("Receiving a request on the XP API")
-        if (controlRequest(req, res, cooldowns, 500)) {
-
-            let { id, limit, page }: ApiQuery = url.parse(req.url, true).query // url.parse is deprecated but there is no easy workaround on the internet and the URL class is terrible to use imo
-
-            if (limit === undefined) limit = 20 // default values
-            if (page === undefined) page = 1
-
-            console.log(`Request accepted. ID = ${id}, limit = ${limit}, page = ${page}`)
-
-            if (isNaN(limit) || limit <= 0 || limit > 100 || isNaN(page) || page <= 0) {
-                res.writeHead(400) // HTTP status code 400 = Bad Request
-
-                const errors = []
-                if (isNaN(limit) || limit <= 0 || limit > 100) {
-                    errors.push("Invalid limit, the limit must be between 1 and 100.")
-                }
-                if (isNaN(page) || page <= 0) {
-                    errors.push("Invalid page, the page must be greater or equal to 1.")
-                }
-
-                res.write(JSON.stringify({ "errors": errors }))
-                return res.end()
-            }
-            else if (id !== undefined) {
-                let guild
-                try {
-                    guild = await bot.guilds.fetch(id)
-                    if (guild.id !== id) id = guild.id // fetch can match a server even if the provided ID is not exactly the same as the server's one (very weird??)
-                }
-                catch {
-                    res.writeHead(404) // HTTP status code 404 = Not Found
-                    res.write(JSON.stringify({ "errors": ["I didn't find the specified server. Please ensure that the ID you provided is a valid server ID and that Kirino has access to it."] }))
-                    return res.end()
-                }
-
-                const serverRequest = bot.db.prepare("SELECT user_id, xp, total_xp, level, color FROM xp_profiles WHERE guild_id = ? ORDER BY level DESC, xp DESC")
-                const serverRows = serverRequest.all(id) as XpProfile[]
-
-                if (serverRows.length > 0) {
-                    const page_start = (page - 1) * limit
-                    const page_end = page * limit - 1
-
-                    if (page_start >= serverRows.length) {
-                        res.writeHead(400) // HTTP status code 400 = Bad Request
-                        res.write(JSON.stringify({ "errors": ["The specified page is out of bounds."] }))
-                        return res.end()
-                    }
-
-                    const data: ApiData = {
-                        guild_metadata: {
-                            id: guild.id,
-                            name: guild.name,
-                            icon: guild.iconURL({ extension: "png", size: 128 }),
-                            players: serverRows.length
-                        },
-                        players: []
-                    }
-
-                    const askedRows = serverRows.slice(page_start, page_end + 1)
-
-                    for (const [i, row] of askedRows.entries()) {
-                        const user = await bot.users.fetch(row.user_id)
-
-                        data.players.push({
-                            id: user.id,
-                            tag: user.displayName,
-                            avatar: user.displayAvatarURL({ size: 128 }),
-                            xp: row.xp,
-                            total_xp: row.total_xp,
-                            level: row.level,
-                            color: row.color,
-                            rank: page_start + i + 1
-                        })
-                    }
-
-                    res.writeHead(200) // HTTP status code 200 = OK
-                    res.write(JSON.stringify(data))
-                    console.log("Request response sent with success")
-                }
-                else {
-                    res.writeHead(400) // HTTP status code 400 = Bad Request
-                    res.write(JSON.stringify({ "errors": ["Nobody has any XP on this server."] }))
-                }
-            }
-            else {
-                res.writeHead(400) // HTTP status code 400 = Bad Request
-                res.write(JSON.stringify({ "errors": ["You must specify the ID of the server you want."] }))
-            }
-            res.end()
-        }
-    }).listen(62150)
-}
---
+++ src/lib/api/api.ts
@@ -0,0 +1,159 @@
+import { StatusCodes } from "http-status-codes"
+import http from "http"
+import url from "url"
+import { Kirino } from "../misc/types"
+import { XpProfile } from "../misc/database"
+
+type ApiQuery = {
+    id?: string,
+    limit?: number,
+    page?: number
+}
+
+type ApiData = {
+    guild_metadata: {
+        id: string,
+        name: string,
+        icon: string | null,
+        players: number
+    },
+    players: {
+        id: string,
+        tag: string,
+        avatar: string,
+        xp: number,
+        total_xp: number,
+        level: number,
+        color: string | undefined,
+        rank: number
+    }[]
+}
+
+function controlRequest(req: http.IncomingMessage, res: http.ServerResponse, apiCooldowns: Map<string, number>, cooldown: number) {
+    res.setHeader("Content-Type", "application/json")
+    if (req.headers.authorization !== process.env.API_TOKEN) {
+        res.writeHead(StatusCodes.FORBIDDEN)
+        res.write(JSON.stringify({ "errors": ["Invalid authentification token."] }))
+        res.end()
+        return false
+    }
+
+    const now = Date.now()
+    const ip = req.socket.remoteAddress
+
+    if (!ip) return false
+
+    const timestamp = apiCooldowns.get(ip)
+    if (timestamp !== undefined) {
+        const expiration = timestamp + cooldown
+
+        if (now < expiration) {
+            res.writeHead(StatusCodes.TOO_MANY_REQUESTS)
+            res.write(JSON.stringify({ "errors": ["Too many requests. Please stop sending requests that fast."] }))
+            res.end()
+            return false
+        }
+    }
+
+    apiCooldowns.set(ip, now)
+    setTimeout(() => apiCooldowns.delete(ip), cooldown)
+    return true
+}
+
+export function startXpApi(bot: Kirino) {
+    http.createServer(async (req, res) => {
+        if (!req.url) return res.end()
+
+        console.log("Receiving a request on the XP API")
+        if (controlRequest(req, res, bot.apiCooldowns, 500)) {
+
+            let { id, limit, page }: ApiQuery = url.parse(req.url, true).query // url.parse is deprecated but there is no easy workaround on the internet and the URL class is terrible to use imo
+
+            if (limit === undefined) limit = 20 // default values
+            if (page === undefined) page = 1
+
+            console.log(`Request accepted. ID = ${id}, limit = ${limit}, page = ${page}`)
+
+            if (isNaN(limit) || limit <= 0 || limit > 100 || isNaN(page) || page <= 0) {
+                res.writeHead(StatusCodes.BAD_REQUEST)
+
+                const errors = []
+                if (isNaN(limit) || limit <= 0 || limit > 100) {
+                    errors.push("Invalid limit, the limit must be between 1 and 100.")
+                }
+                if (isNaN(page) || page <= 0) {
+                    errors.push("Invalid page, the page must be greater or equal to 1.")
+                }
+
+                res.write(JSON.stringify({ "errors": errors }))
+                return res.end()
+            }
+            else if (id !== undefined) {
+                let guild
+                try {
+                    guild = await bot.guilds.fetch(id)
+                    if (guild.id !== id) id = guild.id // fetch can match a server even if the provided ID is not exactly the same as the server's one (very weird??)
+                }
+                catch {
+                    res.writeHead(StatusCodes.NOT_FOUND)
+                    res.write(JSON.stringify({ "errors": ["I didn't find the specified server. Please ensure that the ID you provided is a valid server ID and that Kirino has access to it."] }))
+                    return res.end()
+                }
+
+                const serverRequest = bot.db.prepare("SELECT user_id, xp, total_xp, level, color FROM xp_profiles WHERE guild_id = ? ORDER BY level DESC, xp DESC")
+                const serverRows = serverRequest.all(id) as XpProfile[]
+
+                if (serverRows.length > 0) {
+                    const page_start = (page - 1) * limit
+                    const page_end = page * limit - 1
+
+                    if (page_start >= serverRows.length) {
+                        res.writeHead(StatusCodes.BAD_REQUEST)
+                        res.write(JSON.stringify({ "errors": ["The specified page is out of bounds."] }))
+                        return res.end()
+                    }
+
+                    const data: ApiData = {
+                        guild_metadata: {
+                            id: guild.id,
+                            name: guild.name,
+                            icon: guild.iconURL({ extension: "png", size: 128 }),
+                            players: serverRows.length
+                        },
+                        players: []
+                    }
+
+                    const askedRows = serverRows.slice(page_start, page_end + 1)
+
+                    for (const [i, row] of askedRows.entries()) {
+                        const user = await bot.users.fetch(row.user_id)
+
+                        data.players.push({
+                            id: user.id,
+                            tag: user.displayName,
+                            avatar: user.displayAvatarURL({ size: 128 }),
+                            xp: row.xp,
+                            total_xp: row.total_xp,
+                            level: row.level,
+                            color: row.color,
+                            rank: page_start + i + 1
+                        })
+                    }
+
+                    res.writeHead(StatusCodes.OK)
+                    res.write(JSON.stringify(data))
+                    console.log("Request response sent with success")
+                }
+                else {
+                    res.writeHead(StatusCodes.BAD_REQUEST)
+                    res.write(JSON.stringify({ "errors": ["Nobody has any XP on this server."] }))
+                }
+            }
+            else {
+                res.writeHead(StatusCodes.BAD_REQUEST)
+                res.write(JSON.stringify({ "errors": ["You must specify the ID of the server you want."] }))
+            }
+            res.end()
+        }
+    }).listen(62150)
+}
